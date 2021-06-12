2cd128cb144b0c2e55fc30844bddea14890ce253
Maciej <daraniel1000@gmail.com>
2021-06-12 16:25:07 +0200

    added calculating entropy

--- check.py
+++ check.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 def check(s):
 	original = open(f'input/{s}', 'rb')
@@ -7,15 +8,23 @@ def check(s):
 	ob = original.read(1)
 	db = decoded.read(1)
 	hist = np.zeros((256,))
+	sum = 0
 	while ob and db:
 		if ob != db:
 	        	break;
 		hist[int.from_bytes(ob, 'big')] += 1
+		sum += 1
 		ob = original.read(1)
 		db = decoded.read(1)
 	if ob == db: print(f'{s} is correct')
 	else: print(f'{s} is incorrect')
 	plt.plot(range(256), hist)
 	plt.show()
+	entropy = 0
+	for i in range(256):
+		prob = hist[i]/sum
+		if prob != 0:
+			entropy += prob*math.log2(1/prob)
+	print(f'The entropy of this file is {entropy:.3}')
 	return ob, db, original, decoded
 
